585bd79683d685225fc697074361395cfe07bf51
PierreRudolph <Plettner@hotmail.de>
2023-07-24 11:37:13 +0200

    added user dialog, user input formfields, download and embed fonts and material-icons

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TooltipPosition } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+  positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
+  position = new FormControl(this.positionOptions[2]);
 
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
